713a8f725f632ff1d9dff7bcdf9a35e6cb2c85a2
Dwarakanath Yadavalli <ydnath@users.noreply.github.com>
2020-02-04 22:48:55 +0530

    Update __init__.py

--- jet/__init__.py
+++ jet/__init__.py
@@ -1,8 +1,13 @@
-# Copyright 2018 Juniper Networks, Inc. All rights reserved
+# Copyright 2018 Juniper Networks, Inc.  All rights reserved.
 #
-# Licensed under the Juniper Networks Script Software License (the "License").
-# You may not use this script file except in compliance with the License, which is located at
-# http://www.juniper.net/support/legal/scriptlicense/
-# Unless required by applicable law or otherwise agreed to in writing by the parties,
-# software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
-# OR CONDITIONS OF ANY KIND, either express or implied.
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
